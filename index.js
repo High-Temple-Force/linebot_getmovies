d7675b4eb3fcf5a322c4e730bae6e23cdb08ee27
scsk-mh <dd11blues@gmail.com>
2019-01-28 16:15:09 +0900

    Put id

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
 const { Datastore } = require('@google-cloud/datastore')
+const request = require('request')
+const cheerio = require('cheerio')
 const projectId = 'linebot-get-movies'
 const datastore = new Datastore({
     projectId : projectId,
 })
-
 const kind = 'Movies'
-const name = 'samplemovie1'
-const taskKey = datastore.key([kind, name])
-
-const task = {
-    key: taskKey,
-    data: {
-        name: 'アリー',
-    },
-}
-
-exports.testDATA = (req, res) => {    
-    datastore.save(task).then(() => {
-        res.send(`Saved ${task.key.name} : ${task.data.name}`)
-    })
-    .catch(err => {
-        res.send('Error,' , err)
-    })
-}
-
-const request = require('request')
-const cheerio = require('cheerio')
+const taskKey = datastore.key([kind])
 const url = 'https://eiga.com/now/all/rank/' // 映画.comランキングページ
 const titles_arr = []
 
-request(url, (e, response, body) => {
-    if (e) {
-        console.error(e)
-    }
-    try {
-        const $ = cheerio.load(body)
-        $('h3', '.m_unit' ).each((i, elem) => {
-            titles_arr[i] = $(elem).text()
-        })
-        for (i in titles_arr) {
-            console.log(titles_arr[i])
+const upsertData = (task) => {
+    datastore.upsert(task)
+    .then(console.log('Succsess!'))
+    .catch(err => {
+        console.error(`Failed coz ${err}`)
+    })
+}
+
+exports.testDATA = (req, res) => {    
+    request(url, (e, response, body) => {
+        if (e) {
+            console.error(e)
         }
-     } catch (e) {
-         console.error(e)
+        try {
+            const $ = cheerio.load(body)
+            $('h3', '.m_unit' ).each((i, elem) => {
+                titles_arr[i] = $(elem).text()
+        })
+            for (i in titles_arr) {
+                console.log(titles_arr[i])
+                const taskKey = datastore.key([kind, i])
+                const task = {
+                    key: taskKey,
+                    data: {
+                        name: titles_arr[i]
+                    }
+                }
+                upsertData(task)
+            } 
+        } catch (e) {
+            console.error(e)
      }
-})
+    })
+}
+
+
 
