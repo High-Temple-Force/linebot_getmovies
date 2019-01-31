9ada6c7d05fca0105e08a14762035c4335055c0c
scsk-mh <dd11blues@gmail.com>
2019-01-31 18:49:44 +0900

    Trying to get property

--- index.js
+++ index.js
@@ -1,52 +1,19 @@
-const { Datastore } = require('@google-cloud/datastore')
-const request = require('request')
-const cheerio = require('cheerio')
-const projectId = 'linebot-get-movies'
-const datastore = new Datastore({
-    projectId : projectId,
-})
-const kind = 'Movies'
-const url = 'https://eiga.com/now/all/rank/' // 映画.comランキングページ
-const titles_arr = []
-
-const upsertData = (task) => {
-    datastore.upsert(task)
-    .then(console.log('Succsess!'))
-    .catch(err => {
-        console.error(`Failed coz ${err}`)
-    })
-}
-
-exports.testDATA = (req, res) => {    
-    request(url, (e, response, body) => {
-        if (e) {
-            console.error(e)
-        }
-        try {
-            const $ = cheerio.load(body)
-            $('h3', '.m_unit' ).each((i, elem) => {
-                titles_arr[i] = $(elem).text()
+exports.listTasks = (req, res) => {
+    const query = datastore.createQuery('Movies').order('created')
+    datastore
+      .runQuery(query)
+      .then(results => {
+        const tasks = results[0]
+  
+        console.log('Tasks:')
+        tasks.forEach(task => {
+          const taskKey = task[datastore.KEY]
+          console.log(taskKey.id, task)
         })
-            for (i in titles_arr) {
-                console.log(titles_arr[i])
-                const taskKey = datastore.key([kind, i])
-                const task = {
-                    key: taskKey,
-                    data: {
-                        name: titles_arr[i]
-                    }
-                }
-                upsertData(task)
-               
-            } 
-            res.send('Success!')
-        } catch (e) {
-            console.error(e)
-            res.send('Error! Function ended.')
-     } 
-
-    })
-}
-
-
-
+        res.send('Got it!')
+      })
+      .catch(err => {
+        console.error('ERROR:', err)
+        res.sent('Error!')
+      })
+  }
---
+++ index2.js
@@ -0,0 +1,52 @@
+const { Datastore } = require('@google-cloud/datastore')
+const request = require('request')
+const cheerio = require('cheerio')
+const projectId = 'linebot-get-movies'
+const datastore = new Datastore({
+    projectId : projectId,
+})
+const kind = 'Movies'
+const url = 'https://eiga.com/now/all/rank/' // 映画.comランキングページ
+const titles_arr = []
+
+const upsertData = (task) => {
+    datastore.upsert(task)
+    .then(console.log('Succsess!'))
+    .catch(err => {
+        console.error(`Failed coz ${err}`)
+    })
+}
+
+exports.testDATA = (req, res) => {    
+    request(url, (e, response, body) => {
+        if (e) {
+            console.error(e)
+        }
+        try {
+            const $ = cheerio.load(body)
+            $('h3', '.m_unit' ).each((i, elem) => {
+                titles_arr[i] = $(elem).text()
+        })
+            for (i in titles_arr) {
+                console.log(titles_arr[i])
+                const taskKey = datastore.key([kind, i])
+                const task = {
+                    key: taskKey,
+                    data: {
+                        name: titles_arr[i]
+                    }
+                }
+                upsertData(task)
+               
+            } 
+            res.send('Success!')
+        } catch (e) {
+            console.error(e)
+            res.send('Error! Function ended.')
+     } 
+
+    })
+}
+
+
+
