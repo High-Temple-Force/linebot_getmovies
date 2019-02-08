070cf772883db2ebf3eabf0a4950e8b68d720f9c
scsk-mh <dd11blues@gmail.com>
2019-02-08 10:58:42 +0900

    Editing Logi using Line API

--- index2.js
+++ index2.js
@@ -1,6 +1,10 @@
 const { Datastore } = require('@google-cloud/datastore')
 const request = require('request')
 const cheerio = require('cheerio')
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+
 const projectId = 'linebot-get-movies'
 const datastore = new Datastore({
     projectId : projectId,
@@ -8,6 +12,12 @@ const datastore = new Datastore({
 const kind = 'Movies'
 const url = 'https://eiga.com/now/all/rank/' // 映画.comランキングページ
 const titles_arr = []
+
+
+const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET,
+  }
 
 const upsertData = (task) => {
     datastore.upsert(task)
@@ -50,3 +60,38 @@ exports.testDATA = (req, res) => {
 
 
 
+
+
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
