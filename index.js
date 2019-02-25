b7fcc2ba3b753c460a2c41601bbe54f6d3d8fd00
scsk-mh <dd11blues@gmail.com>
2019-02-25 12:43:38 +0900

    Test

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const datastore = new Datastore({
 
 // configure LINE BOT
 const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelAccessToken: ,
   channelSecret: process.env.CHANNEL_SECRET,
 }
 // create LINE SDK client
@@ -27,7 +27,7 @@ exports.listTasks = (req, res) => {
       .then(results => {
         const tasks = results[0]
         tasks.forEach(task => {
-          console.log(task.name /*, task*/)
+          console.log(task.name)
           titles_arr.push(task.name)
         })
         const titlenames = titles_arr.join('\n')
@@ -47,7 +47,7 @@ exports.listTasks = (req, res) => {
 // about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
   Promise
-    .all(req.body.events.map(handleEvent))
+    .all(req.body.events.map(handleEvent(), titlenames))
     .then((result) => res.json(result))
     .catch((err) => {
       console.error(err)
@@ -57,14 +57,8 @@ app.post('/callback', line.middleware(config), (req, res) => {
 
 // event handler
 const handleEvent = (event) => {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null)
-  }
-
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text }
-
+  const echo = { type: 'text', text: titlenames }
   // use reply API
   return client.replyMessage(event.replyToken, echo)
 }
