628788589d09e92b961081f14e3296f41b9fbb6f
scsk-mh <dd11blues@gmail.com>
2019-02-28 19:23:13 +0900

    Final

--- testpromise.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert')
-
-it('shoud use `done` for test?', function(done) {
-    let promise = Promise.resolve(44)
-    promise.then(function (value) {
-        assert(value === 44)
-        done()
-    })
-})
