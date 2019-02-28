a4f48d700089c110b4e629eb6673c4bcd552ea3f
scsk-mh <dd11blues@gmail.com>
2019-02-28 19:22:25 +0900

    Final

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const datastore = new Datastore({
   projectId : projectId,
 })
 
-
 // configure LINE BOT
 const config = {
   channelAccessToken: 'TxZUc14K+sxqjS297KtOCm5lV4blOzyk/3t7ZE52HWwbZLbZvoYv1jbYmcZWj+t2poBQwq4YOGnlrwRXnT2H7QTaiEcSfNTKv8rHgOnzeJoq7/gJ4h7Q2R13psborwo1IppunKImPJ1nPYrBI4mKAgdB04t89/1O/w1cDnyilFU=',
@@ -15,33 +14,29 @@ const config = {
 // create LINE SDK client
 const client = new line.Client(config)
 
+// event handler to get replytoken
 var handlerEvent = (event) => {
   console.log(event.replyToken)
   return event.replyToken
 }
 
-/*
-app.use('/', line.middleware(config))
-const token = req.body.events[0].replyToken
-res.json(handleEvent(token))
-*/
 exports.listTasks = (req, res) => {
   // query for datastore func
   var Query_Datastore = new Promise ((resolve, reject) => {
     console.log('Start query func')
     datastore
     .runQuery(datastore.createQuery('Movies'))
-    .then((results) => {
+    .then((titles) => {
       var titles_arr = []
-      var tasks = results[0]
+      var tasks = titles[0]
       tasks.forEach((task) => {
         titles_arr.push(task.name)
-      })
-      console.log('Succeeded to get names')
+        })
+      console.log('Succeeded to get names, end query func')
       resolve(titles_arr.join('\n'))
     })
     .catch(err => {
-      reject(console.error('ERROR:', err))
+      reject(console.error('End query func, ERROR:', err))
     })
   })
 
@@ -49,10 +44,16 @@ exports.listTasks = (req, res) => {
   Promise
     .all([req.body.events.map(handlerEvent), Query_Datastore])
     .then((result) => {
-      console.log(result)
       var echo = { type: 'text', text: `${result[1]}` }
       console.log(echo)
-      return client.replyMessage(`${result[0]}`, echo)})
-    .then((result) => res.json(result))
-    .catch((err) => res.status(400).send(err.toString()))
+      return client.replyMessage(`${result[0]}`, echo)
+    })
+    .then((result) => {
+      res.json(result)
+      console.log('Sent movie titles')
+    })
+    .catch((err) => {
+      res.status(400).send(err)
+      console.log('Failed to send movie titles')
+    })
 }
